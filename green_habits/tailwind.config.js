cf37176d57bec80de87771ec2c944ff79798d01c
alexbarnu34 <163245277+alexbarnu34@users.noreply.github.com>
2025-10-27 21:12:25 +0200

    setup react+tailwind

---
+++ green_habits/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+     "./src/**/*.{js,jsx,ts,tsx}", // înlocuiește "green_habits" cu numele real al folderului tău src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
